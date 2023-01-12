16a1a690af60d882154aa02e71ee54e6d6d0a1d3
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-01-12 16:02:13 +0530

    Git Init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on ${DATE},${MONTH_NAME_SHORT},${YEAR}
+ * in Project: ${PROJECT_NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
